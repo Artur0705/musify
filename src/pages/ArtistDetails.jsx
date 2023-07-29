49a06c0f6de91358358ccc841481f821a66fdf8e
Artur0705 <artlil420@gmail.com>
2023-07-29 21:27:22 +1000

    Implemented ArtistDetails component with Redux and Router integration. Modified DetailsHeader component and fixed the issues with the API versions

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import DetailsHeader from "../components/DetailsHeader";
+import Error from "../components/Error";
+import Loader from "../components/Loader";
+import RelatedSongs from "../components/RelatedSongs";
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
 
 const ArtistDetails = () => {
-  return <div>ArtistDetails</div>;
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const {
+    data: artistData,
+    isFetching: isFetchingArtistDetails,
+    error,
+  } = useGetArtistDetailsQuery(artistId);
+
+  if (isFetchingArtistDetails)
+    return <Loader title="Loading artist details..." />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistId} artistData={artistData?.data[0]} />
+
+      <RelatedSongs
+        data={artistData?.data[0].views["top-songs"]?.data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
 };
 
 export default ArtistDetails;
