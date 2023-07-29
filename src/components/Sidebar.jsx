34518ea8fbca713c44220b636864f0e5f741e2d8
Artur0705 <artlil420@gmail.com>
2023-07-29 18:10:01 +1000

    Created a DetailsHeader component to display the primary details of the artist/song. Introduced a RelatedSongs component to showcase songs that are similar to the currently viewed  song. Implemented a SongBar component for a concise song representation, equipped with play/pause functionality. Developed the main SongDetails view, which integrates the aforementioned components and fetches song details and related songs from the ShazamCore API. Added queries to feth the song details and related song data from API. Added onClick function to the logo to navigate to the home page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri";
 import { logo } from "../assets";
 import { links } from "../assets/constants";
@@ -23,11 +23,16 @@ const NavLinks = ({ handleClick }) => (
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
+  const navigate = useNavigate();
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-24 object-contain" />
+        <img
+          onClick={() => navigate("/")}
+          src={logo}
+          alt="logo"
+          className="w-full h-24 object-contain cursor-pointer"
+        />
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
