e382ff3bc8a423967092822fdb1d2b6e4e1a287e
Artur0705 <artlil420@gmail.com>
2023-07-31 23:45:40 +1000

    Minor css updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,29 +31,29 @@ const Sidebar = () => {
           onClick={() => navigate("/")}
           src={logo}
           alt="logo"
-          className="w-full h-24 object-contain cursor-pointer"
+          className="w-full h-14 object-contain cursor-pointer"
         />
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
         {mobileMenuOpen ? (
           <RiCloseLine
-            className="w-6 h-6 text-white mr-2"
+            className="w-6 h-6 mr-2 text-white"
             onClick={() => setMobileMenuOpen(false)}
           />
         ) : (
           <HiOutlineMenu
-            className="w-6 h-6 text-white mr-2"
+            className="w-6 h-6 mr-2 text-white"
             onClick={() => setMobileMenuOpen(true)}
           />
         )}
       </div>
       <div
-        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
           mobileMenuOpen ? "left-0" : "-left-full"
         }`}
       >
-        <img src={logo} alt="logo" className="w-full h-24 object-contain" />
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
