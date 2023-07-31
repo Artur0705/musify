a1412c1638c1a7757a5d02747beabb216c103e12
Artur0705 <artlil420@gmail.com>
2023-07-31 21:26:30 +1000

    Added Search component to display results based on search term, added new query to get the search results from the API and display the results

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,8 +4,16 @@ import { FiSearch } from "react-icons/fi";
 
 const Searchbar = () => {
   const navigate = useNavigate();
+  const [searchTerm, setTermSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searchTerm}`);
+  };
+
   return (
     <form
+      onSubmit={handleSubmit}
       autoComplete="off"
       className="p-2 text-gray-400 focus-within:text-gray-600"
     >
@@ -20,8 +28,8 @@ const Searchbar = () => {
           type="search"
           id="search-field"
           placeholder="Search..."
-          value=""
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setTermSearch(e.target.value)}
           className="flex-1
            bg-transparent
            border-none
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -27,6 +27,10 @@ export const shazamCoreApi = createApi({
     getSongsByGenre: builder.query({
       query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
     }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
   }),
 });
 
@@ -37,4 +41,5 @@ export const {
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
   useGetSongsByGenreQuery,
+  useGetSongsBySearchQuery,
 } = shazamCoreApi;
